c40639eaf381c4f7b4a592c696ea1402afc6b580
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2024-01-14 12:46:14 +1300

    Initial version of files for minimal API projects using Airbnb dataset

---
+++ Listings/Program.cs
@@ -0,0 +1,370 @@
+//---------------------------------------------------------------------------------
+// Copyright (c) Jan 2024, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 see http://www.apache.org/licenses/LICENSE-2.0
+//
+//---------------------------------------------------------------------------------
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.Common;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+using devMobile.Azure.DapperTransient;
+using devMobile.Dapper;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IDapperContext>(s => new DapperContext(builder.Configuration));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+   app.UseSwagger();
+   app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Embedded SQL so queries easy to tweak
+const string LookupByIdSql = @"SELECT Id, [Name], Listing_URL AS ListingURL
+                     FROM ListingsHosts
+                     WHERE id = @Id";
+
+const string SearchCountSql = @"SELECT COUNT(*) FROM ListingsHosts WHERE [Name] LIKE N'%' + @SearchText + N'%'";
+
+const string SearchPaginatedIdSql = @"SELECT Id, [Name], Listing_URL AS ListingURL
+                     FROM ListingsHosts
+                     WHERE[Name] LIKE N'%' + @SearchText + N'%'
+                     ORDER By[Name] 
+                     OFFSET @PageSize *(@PageNumber - 1) ROWS FETCH NEXT @PageSize ROWS ONLY";
+
+
+// The MapGets start here (including ones that don't work so well
+app.MapGet("/Listing/Search/Sync", (string q, int pageNumber, int pageSize, [FromServices] IDapperContext dappperContext) =>
+{
+   using (var connection = dappperContext.ConnectionCreate())
+   {
+      return connection.QueryWithRetry(SearchPaginatedIdSql, new { searchText = q , pageNumber, pageSize }); // IEnumerable<Dynamic>
+   }
+})
+.WithOpenApi();
+
+
+app.MapGet("/Listing/Search/Async", async (string q, int pageNumber, int pageSize, [FromServices] IDapperContext dappperContext) =>
+{
+   using (var connection = dappperContext.ConnectionCreate())
+   {
+      return await connection.QueryWithRetryAsync(SearchPaginatedIdSql, new { searchText = q, pageNumber, pageSize }); // IEnumerable<Dynamic>
+   }
+})
+.WithOpenApi();
+
+
+app.MapGet("/Listing/Search/Typed", async Task<IEnumerable<Model.ListingListDto>> (string q, int pageNumber, int pageSize, [FromServices] IDapperContext dappperContext) =>
+{
+   using (var connection = dappperContext.ConnectionCreate())
+   {
+      return await connection.QueryWithRetryAsync<Model.ListingListDto>(SearchPaginatedIdSql, new { searchText = q, pageNumber, pageSize }); // IEnumerable<ListingListDto>
+   }
+})
+.WithOpenApi();
+
+
+app.MapGet("/Listing/Search/Produces", async (string q, int pageNumber, int pageSize, [FromServices] IDapperContext dappperContext) =>
+{
+   using (var connection = dappperContext.ConnectionCreate())
+   {
+      return await connection.QueryWithRetryAsync<Model.ListingListDto>(SearchPaginatedIdSql, new { searchText = q, pageNumber, pageSize });
+   }
+})
+.Produces<IList<Model.ListingListDto>>(StatusCodes.Status200OK)
+.WithOpenApi();
+
+
+app.MapGet("/Listing/Count", async Task<int> (string q, [FromServices] IDapperContext dappperContext) =>
+{
+   using (var connection = dappperContext.ConnectionCreate())
+   {
+      return await connection.ExecuteScalarWithRetryAsync<int>(SearchCountSql, new { searchText = q});
+   }
+})
+.WithOpenApi();
+
+
+app.MapGet("/Listing/Produces/{id:long}", async (long id, IDapperContext dappperContext) =>
+{
+   using (var connection = dappperContext.ConnectionCreate())
+   {
+      Model.ListingLookupDto result = await connection.QuerySingleOrDefaultWithRetryAsync<Model.ListingLookupDto>(LookupByIdSql, new { id });
+      if (result is null)
+      {
+         return Results.Problem($"Listing {id} not found", statusCode: StatusCodes.Status404NotFound);
+      }
+
+      return Results.Ok(result);
+   }
+})
+.Produces<Model.ListingLookupDto>(StatusCodes.Status200OK)
+.Produces<ProblemDetails>(StatusCodes.Status404NotFound)
+.WithOpenApi();
+
+
+app.MapGet("/Listing/Results/{id:long}", async Task<Results<Ok<Model.ListingLookupDto>, NotFound>> (long id, IDapperContext dappperContext) =>
+{
+   using (var connection = dappperContext.ConnectionCreate())
+   {
+      Model.ListingLookupDto searchResultSet = await connection.QuerySingleOrDefaultWithRetryAsync<Model.ListingLookupDto>(LookupByIdSql, new { id });
+      if (searchResultSet is null)
+      {
+         return TypedResults.NotFound();
+      }
+
+      return TypedResults.Ok(searchResultSet);
+   }
+})
+.WithOpenApi();
+
+
+app.MapGet("/Listing/Search/ValidatedQuery", async (
+   //[FromQuery,Required, MinLength(Constants.SearchTextMinimumLength, ErrorMessage = "SearchTextMaximumLength"), MaxLength(Constants.SearchTextMaximumLength, ErrorMessage = "SearchTextMaximumLegth")]
+   [FromQuery, StringLength(Constants.SearchTextMaximumLength, ErrorMessage = "SearchTextMaximumLength SearchTextMaximumLength",MinimumLength=Constants.SearchTextMinimumLength)]
+   string q,
+   [FromQuery, Range(Constants.PageNumberMinimum, Constants.PageNumberMaximum, ErrorMessage = "PageNumberMinimum PageNumberMaximum")]
+   int pageNumber,
+   [FromQuery, Range(Constants.PageSizeMinimum, Constants.PageSizeMaximum, ErrorMessage = "PageSizeMinimum PageSizeMaximum")]
+   int pageSize,
+   [FromServices] IDapperContext dappperContext) =>
+{
+   using (var connection = dappperContext.ConnectionCreate())
+   {
+      return await connection.QueryWithRetryAsync<Model.ListingListDto>(SearchPaginatedIdSql, new { searchText = q, pageNumber, pageSize });
+   }
+})
+.Produces<IList<Model.ListingListDto>>(StatusCodes.Status200OK)
+.Produces<ProblemDetails>(StatusCodes.Status400BadRequest)
+.WithOpenApi();
+
+
+app.MapGet("/Listing/Search/AsParameters", async ([AsParameters] SearchParameters searchParameters,
+   [FromServices] IDapperContext dappperContext) =>
+{
+   using (var connection = dappperContext.ConnectionCreate())
+   {
+      return await connection.QueryWithRetryAsync<Model.ListingListDto>(SearchPaginatedIdSql, new { searchText = searchParameters.q, searchParameters.PageNumber, searchParameters.PageSize });
+   }
+})
+.Produces<IList<Model.ListingListDto>>(StatusCodes.Status200OK)
+.Produces<ProblemDetails>(StatusCodes.Status400BadRequest)
+.WithOpenApi();
+
+
+app.MapGet("/Listing/Search/DocumentedInCode", async (
+   [FromQuery, Required, MinLength(Constants.SearchTextMinimumLength, ErrorMessage = "SearchTextMinimumLegth"), MaxLength(Constants.SearchTextMaximumLength, ErrorMessage = "SearchTextMaximumLegth")]
+   string q,
+   [Range(Constants.PageNumberMinimum, Constants.PageNumberMaximum, ErrorMessage = "PageNumberMinimum PageNumberMaximum")]
+   int pageNumber,
+   [Range(Constants.PageSizeMinimum, Constants.PageSizeMaximum, ErrorMessage = "PageSizeMinimum PageSizeMaximum")]
+   int pageSize,
+   [FromServices] IDapperContext dappperContext) =>
+{
+   using (var connection = dappperContext.ConnectionCreate())
+   {
+      return await connection.QueryWithRetryAsync<Model.ListingListDto>(SearchPaginatedIdSql, new { searchText = q, pageNumber, pageSize });
+   }
+})
+.Produces<IList<Model.ListingListDto>>(StatusCodes.Status200OK)
+.Produces<ProblemDetails>(StatusCodes.Status400BadRequest)
+.WithName("GetListingsValidated")
+.WithOpenApi(operation => new(operation)
+{
+   Summary = "This is a summary",
+   Description = "This is a description"
+})
+.WithOpenApi(generatedOperation =>
+{
+   var searchTextParameter = generatedOperation.Parameters[0];
+   searchTextParameter.Description = "This is the search text";
+
+   var pageNumberParameter = generatedOperation.Parameters[1];
+   pageNumberParameter.Description = "This is the pageNumber parameter";
+
+   var pageSizeParameter = generatedOperation.Parameters[2];
+   pageSizeParameter.Description = "This is the pageNumber parameter";
+
+   return generatedOperation;
+});
+
+
+app.MapGet("/Listing/Search/AdoSync", ([AsParameters] SearchParameters searchParameters, [FromServices] IDapperContext dappperContext) =>
+{
+   using (DbConnection connection = (DbConnection)dappperContext.ConnectionCreate())
+   {
+      connection.Open();
+
+      using (DbCommand command = connection.CreateCommand())
+      {
+         command.CommandText = SearchPaginatedIdSql;
+
+         var searchTextParameter = command.CreateParameter();
+         searchTextParameter.ParameterName = "SearchText";
+         searchTextParameter.Value = searchParameters.q;
+         searchTextParameter.DbType = DbType.String;
+         command.Parameters.Add(searchTextParameter);
+
+         var pageNumberParameter = command.CreateParameter();
+         pageNumberParameter.ParameterName = "PageNumber";
+         pageNumberParameter.Value = searchParameters.PageNumber;
+         pageNumberParameter.DbType = DbType.Byte;
+         command.Parameters.Add(pageNumberParameter);
+
+         var pageSizeParameter = command.CreateParameter();
+         pageSizeParameter.ParameterName = "PageSize";
+         pageSizeParameter.Value = searchParameters.PageSize;
+         pageSizeParameter.DbType = DbType.Byte;
+         command.Parameters.Add(pageSizeParameter);
+
+         using (var dbDataReader = command.ExecuteReader())
+         {
+            List<Model.ListingListDto> listings = new List<Model.ListingListDto>();
+
+            int idColumn = dbDataReader.GetOrdinal("Id");
+            int nameColumn = dbDataReader.GetOrdinal("Name");
+            int listingUrlColumn = dbDataReader.GetOrdinal("ListingURL");
+
+            while (dbDataReader.Read())
+            {
+               listings.Add(new Model.ListingListDto
+               {
+                  Id = (ulong)dbDataReader.GetInt64(idColumn),
+                  Name = dbDataReader.GetString(nameColumn),
+                  ListingURL = dbDataReader.GetString(listingUrlColumn),
+               });
+            }
+
+            return listings;
+         }
+      }
+   }
+})
+.Produces<IList<Model.ListingListDto>>(StatusCodes.Status200OK)
+.Produces<ProblemDetails>(StatusCodes.Status400BadRequest)
+.WithOpenApi();
+
+
+app.MapGet("/Listing/Search/AdoAsync", async ([AsParameters] SearchParameters searchParameters, [FromServices] IDapperContext dappperContext) =>
+{
+   using (DbConnection connection = (DbConnection)dappperContext.ConnectionCreate())
+   {
+      await connection.OpenAsync();
+
+      using (DbCommand command = connection.CreateCommand())
+      {
+         command.CommandText = SearchPaginatedIdSql;
+
+         var searchTextParameter = command.CreateParameter();
+         searchTextParameter.ParameterName = "SearchText";
+         searchTextParameter.Value = searchParameters.q;
+         searchTextParameter.DbType = DbType.String;
+         command.Parameters.Add(searchTextParameter);
+
+         var pageNumberParameter = command.CreateParameter();
+         pageNumberParameter.ParameterName = "PageNumber";
+         pageNumberParameter.Value = searchParameters.PageNumber;
+         pageNumberParameter.DbType = DbType.Byte;
+         command.Parameters.Add(pageNumberParameter);
+
+         var pageSizeParameter = command.CreateParameter();
+         pageSizeParameter.ParameterName = "PageSize";
+         pageSizeParameter.Value = searchParameters.PageSize;
+         pageSizeParameter.DbType = DbType.Byte;
+         command.Parameters.Add(pageSizeParameter);
+
+         using (var dbDataReader = await command.ExecuteReaderAsync())
+         {
+            List<Model.ListingListDto> listings = new List<Model.ListingListDto>();
+
+            int idColumn = dbDataReader.GetOrdinal("Id");
+            int nameColumn = dbDataReader.GetOrdinal("Name");
+            int listingUrlColumn = dbDataReader.GetOrdinal("ListingURL");
+
+            while (await dbDataReader.ReadAsync())
+            {
+               listings.Add(new Model.ListingListDto
+               {
+                  Id = (ulong)dbDataReader.GetInt64(idColumn),
+                  Name = dbDataReader.GetString(nameColumn),
+                  ListingURL = dbDataReader.GetString(listingUrlColumn),
+               });
+
+               //yield return rowParser(reader); // Need a non lambda version so can use yield
+            }
+
+            return listings;
+         }
+      }
+   }
+})
+.Produces<IList<Model.ListingListDto>>(StatusCodes.Status200OK)
+.Produces<ProblemDetails>(StatusCodes.Status400BadRequest)
+.WithOpenApi();
+
+app.Run();
+
+
+public record Constants
+{
+   public const byte SearchTextMinimumLength = 3;
+   public const byte SearchTextMaximumLength = 20;
+   public const byte PageNumberMinimum = 1;
+   public const byte PageNumberMaximum = 100;
+   public const byte PageSizeMinimum = 5;
+   public const byte PageSizeMaximum = 50;
+}
+
+
+public record SearchParameters
+{
+   // https://github.com/domaindrivendev/Swashbuckle.AspNetCore/issues/2658 possibly related?
+
+   //[FromQuery, Required, MinLength(Constants.SearchTextMinimumLength, ErrorMessage = "SearchTextMinimumLegth"), MaxLength(Constants.SearchTextMaximumLength, ErrorMessage = "SearchTextMaximumLegth")]
+   //[Required, MinLength(Constants.SearchTextMinimumLength, ErrorMessage = "SearchTextMinimumLegth"), MaxLength(Constants.SearchTextMaximumLength, ErrorMessage = "SearchTextMaximumLegth")]
+   //[MinLength(Constants.SearchTextMinimumLength, ErrorMessage = "SearchTextMinimumLegth"), MaxLength(Constants.SearchTextMaximumLength, ErrorMessage = "SearchTextMaximumLegth")]
+   [StringLength(Constants.SearchTextMaximumLength, ErrorMessage = "SearchTextMaximumLength SearchTextMaximumLength", MinimumLength = Constants.SearchTextMinimumLength)]
+   public string q { get; set; }
+
+   //[FromQuery, Range(Constants.PageNumberMinimum, Constants.PageNumberMaximum, ErrorMessage = "PageNumberMinimum PageNumberMaximum")]
+   //[Required, Range(Constants.PageNumberMinimum, Constants.PageNumberMaximum, ErrorMessage = "PageNumberMinimum PageNumberMaximum")]
+   [Range(Constants.PageNumberMinimum, Constants.PageNumberMaximum, ErrorMessage = "PageNumberMinimum PageNumberMaximum")]
+   public int PageNumber { get; set; }
+
+   [Range(Constants.PageSizeMinimum, Constants.PageSizeMaximum, ErrorMessage = "PageSizeMinimum PageSizeMaximum")]
+   public int PageSize { get; set; }
+}
+
+namespace Model
+{
+   public record ListingListDto
+   {
+      public ulong Id { get; set; }
+      [Required]
+      public string? Name { get; set; }
+      [Required]
+      public string? ListingURL { get; set; }
+   };
+
+   public record ListingLookupDto
+   {
+      public long Id { get; set; }
+      [Required]
+      public string? Name { get; set; }
+      [Required]
+      public string? ListingURL { get; set; }
+   };
+}
---
+++ NeighbourhoodGeoJsonLoader/Program.cs
@@ -0,0 +1,102 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) Jan 2024, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 see http://www.apache.org/licenses/LICENSE-2.0
+//
+//---------------------------------------------------------------------------------
+/*
+https://bertwagner.com/tag/geojson.html
+
+https://datatracker.ietf.org/doc/html/rfc7946
+3.1.6.  Polygon
+To specify a constraint specific to Polygons, it is useful to introduce the concept of a linear ring:
+
+   o A linear ring is a closed LineString with four or more positions.
+
+   o  The first and last positions are equivalent, and they MUST contain
+      identical values; their representation SHOULD also be identical.
+
+   o  A linear ring is the boundary of a surface or the boundary of a
+      hole in a surface.
+
+   o  A linear ring MUST follow the right-hand rule with respect to the
+      area it bounds, i.e., exterior rings are counterclockwise, and
+      holes are clockwise.
+
+https://learn.microsoft.com/en-us/sql/relational-databases/spatial/polygon?view=sql-server-ver16
+
+he interior of the polygon in an ellipsoidal system is defined by the "left-hand rule": if you imagine yourself 
+walking along the ring of a geography Polygon, following the points in the order in which they are listed, the 
+area on the left is being treated as the interior of the Polygon, and the area on the right as the exterior of 
+the Polygon.
+
+*/
+using System.Text.Json;
+using System.Data;
+using System.Data.SqlClient;
+
+using Microsoft.SqlServer.Types;
+
+using Dapper;
+
+string jsonString = File.ReadAllText("neighbourhoods.geojson");
+
+
+using (IDbConnection connection = new SqlConnection("This is not the connection string you are looking for"))
+{
+   connection.Open();
+
+   var neighbourHoods = JsonSerializer.Deserialize<GeoJSON.Text.Feature.FeatureCollection>(jsonString)!;
+
+   Console.WriteLine($"Features:{neighbourHoods.Features.Count}");
+   foreach (var feature in neighbourHoods.Features)
+   {
+      string neighbourhood = feature.Properties["neighbourhood"].ToString();
+
+      Console.WriteLine($"Neightbourhood:{neighbourhood}");
+
+      var geometery = (GeoJSON.Text.Geometry.MultiPolygon)feature.Geometry;
+
+      var s = new SqlGeographyBuilder();
+
+      s.SetSrid(4326);
+
+      s.BeginGeography(OpenGisGeographyType.MultiPolygon);
+      s.BeginGeography(OpenGisGeographyType.Polygon); // A
+      
+      Console.WriteLine($"Polygon cordinates:{geometery.Coordinates.Count}");
+      foreach (var coordinates in geometery.Coordinates)
+      {
+        //s.BeginGeography(OpenGisGeographyType.Polygon); // B
+
+         Console.WriteLine($"Linestring cordinates:{coordinates.Coordinates.Count}");
+         foreach (var c in coordinates.Coordinates)
+         {
+            Console.WriteLine($"Point cordinates:{c.Coordinates.Count}");
+
+            s.BeginFigure(c.Coordinates[0].Latitude, c.Coordinates[0].Longitude, null, null);
+
+            for (int i = 1; i < c.Coordinates.Count; i++)
+            {
+               s.AddLine(c.Coordinates[i].Latitude, c.Coordinates[i].Longitude);
+
+               Console.Write('.');
+            }
+            Console.WriteLine();
+
+            s.EndFigure();
+         }
+         //s.EndGeography(); //B
+      }
+
+      s.EndGeography(); //A
+      s.EndGeography(); // OpenGisGeographyType.MultiPolygon
+
+      connection.Execute("INSERT INTO Neighbourhood (Name, Boundary) VALUES(@Neighbourhood, geography::STMPolyFromText(@boundary, 4326))", new { neighbourhood, boundary = s.ConstructedGeography.ToString()});
+
+      Console.WriteLine();
+   }
+}
+
+Console.WriteLine("loaded press <enter> to exit");
+Console.ReadLine();
