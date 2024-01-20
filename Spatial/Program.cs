0ca089e76674d8c16a72a5d1d9d140f1d55571da
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2024-01-20 19:44:20 +1300

    First cut of spatial minimal API

--- Spatial/Program.cs
+++ Spatial/Program.cs
@@ -1,13 +1,26 @@
+//---------------------------------------------------------------------------------
+// Copyright (c) Jan 2024, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 see http://www.apache.org/licenses/LICENSE-2.0
+//
+//---------------------------------------------------------------------------------
+using System.ComponentModel.DataAnnotations;
+
+using Microsoft.AspNetCore.Mvc;
+
+using devMobile.Azure.DapperTransient;
+using devMobile.Dapper;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddTransient<IDapperContext>(s => new DapperContext(builder.Configuration));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
    app.UseSwagger();
@@ -16,29 +29,82 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-   var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-       .ToArray();
-   return forecast;
+const string ListingNeighbourhoodSQL = @"SELECT Id, neighbourhoodUID, name, neighbourhood_url FROM Neighbourhood ORDER BY Name";
+
+const string ListingInNeighbourhoodSQL = @"SELECT Id, neighbourhoodUID, name, neighbourhood_url FROM Neighbourhood WHERE Neighbourhood.Boundary.STContains(geography::Point(@Latitude, @Longitude, 4326)) = 1";
+
+const string ListingnearbySQL = @"DECLARE @Origin AS GEOGRAPHY = geography::Point(@Latitude, @Longitude, 4326); 
+                                  DECLARE @Circle AS GEOGRAPHY = @Origin.STBuffer(@distance); 
+
+                                 SELECT ID, name, description, @Origin.STDistance(Listing.Location) as Distance 
+                                 FROM [listing] 
+                                 WHERE Listing.Location.STWithin(@Circle) = 1 ORDER BY Distance";
+
+
+app.MapGet("/Spatial/Neighbourhoods", async ( [FromServices] IDapperContext dappperContext) =>
+{ 
+   using (var connection = dappperContext.ConnectionCreate())
+   {
+      return await connection.QueryWithRetryAsync<Model.NeightbourhoodListDto>(ListingNeighbourhoodSQL );
+   }
 })
-.WithName("GetWeatherForecast")
+.Produces<IList<Model.NeightbourhoodListDto>>(StatusCodes.Status200OK)
+.WithOpenApi();
+
+app.MapGet("/Spatial/Neighbourhood", async (double latitude, double longitude, [FromServices] IDapperContext dappperContext) =>
+{
+   using (var connection = dappperContext.ConnectionCreate())
+   {
+      return await connection.QuerySingleOrDefaultWithRetryAsync<Model.NeightbourhoodSearchDto>(ListingInNeighbourhoodSQL, new { latitude, longitude });
+   }
+})
+.Produces<IList<Model.NeightbourhoodListDto>>(StatusCodes.Status200OK)
+.WithOpenApi();
+
+app.MapGet("/Spatial/Nearby", async (double latitude, double longitude, double distance, [FromServices] IDapperContext dappperContext) =>
+{
+   using (var connection = dappperContext.ConnectionCreate())
+   {
+      return await connection.QueryWithRetryAsync<Model.ListingNearbyListDto>(ListingnearbySQL, new { latitude, longitude, distance });
+   }
+})
+.Produces<IList<Model.ListingNearbyListDto>>(StatusCodes.Status200OK)
 .WithOpenApi();
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+namespace Model
 {
-   public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+   internal record NeightbourhoodListDto
+   {
+      public ulong Id { get; set; }
+      public Guid NeighbourhoodUID { get; set; }
+      [Required]
+      public string? Name { get; set; }
+      [Required]
+      public string? Neighbourhood_url { get; set; }
+   };
+
+   internal record NeightbourhoodSearchDto
+   {
+      public ulong Id { get; set; }
+      public Guid NeighbourhoodUID { get; set; }
+      [Required]
+      public string? Name { get; set; }
+      [Required]
+      public string? Neighbourhood_url { get; set; }
+   };
+
+   internal record ListingNearbyListDto
+   {
+      public Guid NeighbourhoodUID { get; set; }
+      public ulong Id { get; set; }
+      [Required]
+      public string? Name { get; set; }
+      [Required]
+      public double Distance { get; set; }
+      [Required]
+      public string? Neighbourhood_url { get; set; }
+   };
 }
